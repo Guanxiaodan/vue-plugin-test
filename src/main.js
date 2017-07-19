2d8a7be615002703e8a5b09ac6ccdf94aa979d3f
关晓丹 <GXD9983@163.com>
2017-07-19 11:06:46 +0800

    vcharts可以使用

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VeLine from 'v-charts/lib/line'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.component(VeLine.name, VeLine)
 
 /* eslint-disable no-new */
 new Vue({
